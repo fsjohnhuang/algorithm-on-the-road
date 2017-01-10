8169bc4cb74fa0d39eb8b8ede8d023a2560c302e
fsjohnhuang <fsjohnhuang@hotmail.com>
2017-01-10 22:47:41 +0800

    fix c05/s01_dfs

--- aha_algorithm/c05/s01_dfs.c
+++ aha_algorithm/c05/s01_dfs.c
@@ -10,14 +10,14 @@ typedef struct {
 	EdgeType *edges; // 邻接矩阵(边表)
 } MGraph;
 
-MGraph *create_mgraph(const unsigned int vex_num){
+MGraph *create_mgraph(const unsigned int vex_num, char *vexs){
 	MGraph *g = (MGraph *)malloc(sizeof(MGraph));
 	g->vex_num = vex_num;
 	g->vexs = (VertexType *)calloc(vex_num, sizeof(VertexType));
 	g->edges = (EdgeType *)calloc(pow(vex_num, 2), sizeof(EdgeType));
 
 	for (int i = 0; i < vex_num; i++) {
-		*(g->vexs+i) = i+1;
+		*(g->vexs+i) = *(vexs+i);
 	}
 
 	return g;
@@ -32,7 +32,7 @@ void draw_arcs(MGraph *g){
 		1, 0, 1, 1, 0
 	};
 
-	int count = sizeof(arcs)/sizeof(unsigned int);
+	int count = pow(g->vex_num, 2);
 	for (int i = 0; i < count; i++) {
 		*(g->edges+i) = arcs[i];
 	}
@@ -43,27 +43,32 @@ void dfs(MGraph *g, int *vex_walked, const unsigned int seq, VertexType vex){
 	if (*(vex_walked+pos) != -1) return;
 
 	*(vex_walked+pos) = seq;
-	while (pos >= 0 && pos < 5*5) {
+	int count = pow(g->vex_num, 2);
+	while (pos >= 0 && pos < count) {
 		if (*(g->edges+pos) == 1){
 			dfs(g, vex_walked, seq + 1, pos/5 + 1);
 		}
-		pos += 5;
+		pos += g->vex_num;
 	}
 }
 
 int main(int argc, int **argv){
-	MGraph *g = create_mgraph(5);
+	char vexs[] = {'A', 'B', 'C', 'D', 'E'};
+	MGraph *g = create_mgraph(sizeof(vexs)/sizeof(char), vexs);
 	draw_arcs(g);
 
-	int vex_walked[] = {-1, -1, -1, -1, -1};
+	int vex_walked[g->vex_num];
+	for (int i = 0; i < g->vex_num; i++) {
+		vex_walked[i] = -1;
+	}
 	dfs(g, vex_walked, 0, 1);
 
-	int result[5];
-	for (int i = 0; i < 5; i++) {
-		result[vex_walked[i]] = i+1;
+	char result[g->vex_num];
+	for (int i = 0; i < g->vex_num; i++) {
+		result[vex_walked[i]] = *(g->vexs+i);
 	}
-	for (int i = 0; i < 5; i++) {
-		printf("%d\n", result[i]);
+	for (int i = 0; i < g->vex_num; i++) {
+		printf("%c", result[i]);
 	}
 
 	free(g);
